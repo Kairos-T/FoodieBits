1c18218f8b56d9b84352a087cf77e4d542b2a888
Kairos Tay <hestiatay@gmail.com>
2023-12-31 18:44:43 +0800

    fix: import/export issue

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,11 @@
 // Constants for the application for code reusability and maintainability.
 // Arrays here, for instance, are iterated over in the application.
 
-export const appInfo = {
+ const appInfo = {
     logoText: "FoodieBits",
-    description:{
-        title: "Welcome to FoodieBits!",
-        content: "FoodieBits: Your one-stop destination for all things food. Discover diverse food choices, unravel the art of cooking, and lean into the foodie lifestyle! From mouthwatering visuals to food recommendations, we have it all, we have it all! Feast your eyes, indulge your senses, and let every bite be a story. FoodieBits - where flavour meets fascination!"
-    },
-};
+    content: "Your one-stop destination for all things food. Discover diverse food choices, unravel the art of cooking, and lean into the foodie lifestyle! From mouthwatering visuals to food recommendations, we have it all, we have it all! Feast your eyes, indulge your senses, and let every bite be a story. FoodieBits - where flavour meets fascination!",
+}
+
+export{
+    appInfo
+}
