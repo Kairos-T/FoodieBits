653306eceaeccc955eee995d0aa5881969cd6a47
Kairos Tay <hestiatay@gmail.com>
2023-12-31 18:45:04 +0800

    fix: wrong import method

--- src/components/headtag.jsx
+++ src/components/headtag.jsx
@@ -3,19 +3,11 @@
 
 import React from "react";
 import Head from "next/head";
-import appInfo from "../constants";
+import { appInfo } from "../constants";
 
 const HeadTag = ({ page }) => {
   <Head>
     <title>{`${appInfo.logoText} | ${page}`}</title>
-    <link rel="icon" href="/favicon.ico" />
-    <meta name="title" content={userinfo.logoText} />
-    <meta name="description" content={userinfo.content} />
-
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content={userinfo.logoText} />
-    <meta property="og:description" content={userinfo.content} />
-    <meta property="og:image" content="" />
   </Head>;
 };
 
