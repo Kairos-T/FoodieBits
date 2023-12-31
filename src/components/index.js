1607e160fccef29f8369bd1a7ff173d897fcefc3
Kairos Tay <hestiatay@gmail.com>
2023-12-31 18:45:42 +0800

    feat: added import statements for present components

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,16 @@
 // Consolidate all components into this file
 
 // Import components
+import HeadTag from "./headtag";
 import Navbar from "./navbar";
+import HomePage from "./home";
 
 import Footer from "./footer";
 
 // Export components
 export { 
+    HeadTag,
     Navbar,
+    HomePage,
     Footer,
  };
