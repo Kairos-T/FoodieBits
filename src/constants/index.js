5737bb86cb1c4148a8cf183430a523b8735dedce
Kairos Tay <hestiatay@gmail.com>
2024-01-01 15:02:42 +0800

    feat: added structure of constants, used in layout

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,8 @@
-// Constants for the application for code reusability and maintainability. 
-// Arrays here, for instance, are iterated over in the application. 
+// Constants for the application for code reusability and maintainability.
+// Arrays here, for instance, are iterated over in the application.
+
+export const appInfo = {
+  appTitle: "FoodieBits",
+  appDesc:
+    "Your one-stop destination for all things food. Dive into the world of culinary delights, explore a myriad of food choices, and unravel the stories behind your favourite dishes! FoodieBits - Where every bite is an adventure waiting to be savoured!",
+};
