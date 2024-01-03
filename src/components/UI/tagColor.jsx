6fdb101ee21878e6927dfcbd98c2938bc560eac3
Kairos Tay <hestiatay@gmail.com>
2024-01-03 21:32:48 +0800

    feat: edited colours of tags

--- src/components/UI/tagColor.jsx
+++ src/components/UI/tagColor.jsx
@@ -1,18 +1,18 @@
 // Tags for Recipes
 // By: Kairos
 export const tagColor = {
-  Easy: { bgColor: "#38830a", hover: "#09731a" },
+  Easy: { bgColor: "#86c34c", hover: "#5a8d33" }, // Greenish
   Medium: { bgColor: "#f48d07", hover: "#da7009" },
-  Hard: { bgColor: "#3178C6E2", hover: "#265d99E2" },
-  Quick: { bgColor: "#E8265EE2", hover: "#ce0840E2" },
-  Moderate: { bgColor: "#1A91DAE2", hover: "#0b7bbcE2" },
+  Hard: { bgColor: "#E8265EE2", hover: "#ce0840E2" },
+  Quick: { bgColor: "#1DAC5AFF", hover: "#18904b" },
+  Moderate: { bgColor: "#f4ad07", hover: "#dc9406" },
   "Time Consuming": { bgColor: "#b2475dE2", hover: "#933346E2" },
   Italian: { bgColor: "#865DCAE2", hover: "#784cc4E2" },
   Mexican: { bgColor: "#f11df1E2", hover: "#d313d3E2" },
   Indian: { bgColor: "#f06529", hover: "#e34c26" },
   Western: { bgColor: "#ff7bacE2", hover: "#f7659aE2" },
-  Chinese: { bgColor: "#f96624", hover: "#f75b1d" },
+  Chinese: { bgColor: "#f96624", hover: "#e45218" },
   Vegetarian: { bgColor: "#77aa7bE2", hover: "#609363E2" },
-  Vegan: { bgColor: "#FE4A49E2", hover: "#ed3434E2" },
+  Vegan: { bgColor: "#86c34c", hover: "#5a8d33" }, // Greenish
   Keto: { bgColor: "#05afa0E2", hover: "#0ce5d2E2" }
 };
