23e6ff71546936d99aef5940eba35ece235ef627
Kairos Tay <hestiatay@gmail.com>
2024-01-05 20:24:52 +0800

    fix: edited image path for index imgs

--- config.js
+++ config.js
@@ -10,19 +10,19 @@ export const data = [
     title: "Your title",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla tempora dolorem doloribus repudiandae, possimus quod quas. Ipsum culpa repellat dolorem vero odit iste delectus id, sed iure facere, animi suscipit.",
-    image: "/code-review-bro.png"
+    image: "/images/code-review-bro.png"
   },
   {
     title: "Your title",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla tempora dolorem doloribus repudiandae, possimus quod quas. Ipsum culpa repellat dolorem vero odit iste delectus id, sed iure facere, animi suscipit.",
-    image: "/code-review-bro.png"
+    image: "/images/code-review-bro.png"
   },
   {
     title: "Your title",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla tempora dolorem doloribus repudiandae, possimus quod quas. Ipsum culpa repellat dolorem vero odit iste delectus id, sed iure facere, animi suscipit.",
-    image: "/code-review-bro.png"
+    image: "/images/code-review-bro.png"
   }
 ];
 
