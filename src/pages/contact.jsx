9dccf7bf1915768da21feb2fecdb468b2d8fbbea
Kairos Tay <hestiatay@gmail.com>
2024-01-05 20:20:46 +0800

    feat: edited contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,9 @@
+// By: Kairos and Bowen
+
 import { Box, Heading, Text, useColorModeValue } from "@chakra-ui/react";
 import { NextSeo } from "next-seo";
 import { seo } from "config";
 import Faq from "../components/faq";
-
 
 const Contact = ({}) => {
   const title = "Contact";
@@ -36,10 +37,24 @@ const Contact = ({}) => {
         </Heading>
       </Box>
       <Faq />
+      <Box
+        as="section"
+        display="flex"
+        alignItems="center"
+        flexDir="column"
+        textAlign="center"
+        py="4"
+      >
+        {" "}
+        <Heading as="h1" color={color} fontSize="4xl" fontWeight="700" py="2">
+          Contact Us{" "}
+        </Heading>
+        <Text py="4">
+           Do you have questions unanswered, want collaborate with us or have suggestions? Drop us an email below and our team will get back to you within 3 days! While waiting, why not check out some of our FAQs?
+        </Text>
+      </Box>
 
     </>
   );
 };
-
-
 export default Contact;
