82ef84c713eba1980eebcba8357a8f2eff8118f9
Kairos Tay <hestiatay@gmail.com>
2024-01-06 22:35:27 +0800

    chore: removed placeholder recipes content

--- src/pages/cuisines.jsx
+++ src/pages/cuisines.jsx
@@ -1,145 +1,49 @@
-import { useCallback, useEffect, useState } from "react";
-import { Alert, AlertIcon, Box, Flex, Input } from "@chakra-ui/react";
-import { useRouter } from "next/router";
 import { NextSeo } from "next-seo";
-import Fuse from "fuse.js";
 import { motion } from "framer-motion";
-
-import { getAllFilesFrontMatter } from "@/lib/posts";
-import { tagColor } from "@/components/UI/tagColor";
 import { seo } from "config";
-import TagComponent from "@/components/UI/tag";
-import RecipePost from "@/components/recipePost";
+import { Box, Heading, Text, useColorModeValue } from "@chakra-ui/react";
 
-const options = {
-  includeScore: true,
-  threshold: 0.3,
-  ignoreLocation: true,
-  keys: ["title"]
-};
-
-const Blog = ({ posts }) => {
-  const router = useRouter();
-
-  const fuse = new Fuse(posts, options);
-
-  const [blogPost, setBlogPost] = useState(posts);
-  const [searchValue, setSearchValue] = useState("");
-
-  const filteredPosts = (tag) => {
-    const blogResults = posts.filter((post) => post.tags.includes(tag));
-    setBlogPost(blogResults);
-  };
-
-  const updateSearch = () => {
-    const results = fuse.search(searchValue);
-    const blogResults = results.map((res) => res.item);
-    setBlogPost(blogResults);
-  };
-
-  const delayedSearch = useCallback(updateSearch, [searchValue]);
-
-  useEffect(() => {
-    if (searchValue.length === 0) {
-      return setBlogPost(posts);
-    }
-    delayedSearch();
-  }, [delayedSearch]);
-
-  useEffect(() => {
-    if (router.query?.tag !== undefined) {
-      filteredPosts(router.query?.tag);
-    }
-  }, [router]);
-
-  const title = "Blog";
+const Cuisines = ({}) => {
+  const title = "Cuisines";
   const description = seo.description;
-  const url = `${seo.canonical}blog`;
+  const url = `${seo.canonical}cuisines`;
+  const color = useColorModeValue("telegram.500", "telegram.400");
 
   return (
     <>
       <motion.main
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 0.4 }}
-      ><NextSeo
-        title={title}
-        description={description}
-        canonical={url}
-        openGraph={{
-          title,
-          description,
-          url
-        }}
-      />
-
+        transition={{ duration: 0.4 }}>
+        <NextSeo
+          title={title}
+          description={description}
+          canonical={url}
+          openGraph={{
+            title,
+            description,
+            url
+          }}
+        />
         <Box
           as="section"
-          fontSize="16px"
-          px={{ md: "10", lg: "20", xl: "30" }}
+          display="flex"
+          alignItems="center"
+          flexDir="column"
+          textAlign="center"
           py="4"
         >
-          <Flex justify="center">
-            <Input
-              onChange={(e) => {
-                setSearchValue(e.target.value);
-              }}
-              value={searchValue}
-              variant="outline"
-              placeholder="Search..."
-              maxWidth="400px"
-            />
-          </Flex>
+          {" "}
+          <Heading as="h1" color={color} fontSize="4xl" fontWeight="700" py="2">
+            Cuisines{" "}
+          </Heading>
+          <Text py="4">
 
-          <Flex
-            justify="center"
-            align="center"
-            direction="row"
-            wrap="wrap"
-            m="1.5rem 0"
-          >
-            {Object.keys(tagColor).map((tag, index) => {
-              const color = tagColor[tag];
-
-              return (
-                <Box key={index}>
-                  <TagComponent color={color} onClick={() => filteredPosts(tag)}>
-                    {tag}
-                  </TagComponent>
-                </Box>
-              );
-            })}
-          </Flex>
-
-          {blogPost.length > 0 ? (
-            <RecipePost posts={blogPost} />
-          ) : (
-            <Alert
-              status="info"
-              borderRadius="md"
-              display="flex"
-              justifyContent="center"
-              mx="auto"
-              maxWidth="500px"
-              fontWeight="500"
-            >
-              <AlertIcon />
-              No post has been found!
-            </Alert>
-          )}
+          </Text>
         </Box>
       </motion.main>
     </>
   );
 };
 
-export async function getStaticProps() {
-  const data = await getAllFilesFrontMatter("recipes");
-  const posts = data.sort(
-    (a, b) => Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
-  );
-
-  return { props: { posts } };
-}
-
-export default Blog;
+export default Cuisines;
--- src/pages/restaurants.jsx
+++ src/pages/restaurants.jsx
@@ -1,68 +1,20 @@
-import { useCallback, useEffect, useState } from "react";
-import { Alert, AlertIcon, Box, Flex, Input } from "@chakra-ui/react";
-import { useRouter } from "next/router";
 import { NextSeo } from "next-seo";
-import Fuse from "fuse.js";
 import { motion } from "framer-motion";
-
-import { getAllFilesFrontMatter } from "@/lib/posts";
-import { tagColor } from "@/components/UI/tagColor";
 import { seo } from "config";
-import TagComponent from "@/components/UI/tag";
-import RecipePost from "@/components/recipePost";
+import { Box, Heading, Text, useColorModeValue } from "@chakra-ui/react";
 
-const options = {
-  includeScore: true,
-  threshold: 0.3,
-  ignoreLocation: true,
-  keys: ["title"]
-};
-
-const Blog = ({ posts }) => {
-  const router = useRouter();
-
-  const fuse = new Fuse(posts, options);
-
-  const [recipePost, setrecipePost] = useState(posts);
-  const [searchValue, setSearchValue] = useState("");
-
-  const filteredPosts = (tag) => {
-    const blogResults = posts.filter((post) => post.tags.includes(tag));
-    setrecipePost(blogResults);
-  };
-
-  const updateSearch = () => {
-    const results = fuse.search(searchValue);
-    const blogResults = results.map((res) => res.item);
-    setrecipePost(blogResults);
-  };
-
-  const delayedSearch = useCallback(updateSearch, [searchValue]);
-
-  useEffect(() => {
-    if (searchValue.length === 0) {
-      return setrecipePost(posts);
-    }
-    delayedSearch();
-  }, [delayedSearch]);
-
-  useEffect(() => {
-    if (router.query?.tag !== undefined) {
-      filteredPosts(router.query?.tag);
-    }
-  }, [router]);
-
-  const title = "Blog";
+const Restaurants = ({}) => {
+  const title = "Restaurants";
   const description = seo.description;
-  const url = `${seo.canonical}blog`;
+  const url = `${seo.canonical}restaurants`;
+  const color = useColorModeValue("telegram.500", "telegram.400");
 
   return (
     <>
       <motion.main
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 0.4 }}
-      >
+        transition={{ duration: 0.4 }}>
         <NextSeo
           title={title}
           description={description}
@@ -73,74 +25,25 @@ const Blog = ({ posts }) => {
             url
           }}
         />
-
         <Box
           as="section"
-          fontSize="16px"
-          px={{ md: "10", lg: "20", xl: "30" }}
+          display="flex"
+          alignItems="center"
+          flexDir="column"
+          textAlign="center"
           py="4"
         >
-          <Flex justify="center">
-            <Input
-              onChange={(e) => {
-                setSearchValue(e.target.value);
-              }}
-              value={searchValue}
-              variant="outline"
-              placeholder="Search..."
-              maxWidth="400px"
-            />
-          </Flex>
+          {" "}
+          <Heading as="h1" color={color} fontSize="4xl" fontWeight="700" py="2">
+            Restaurants{" "}
+          </Heading>
+          <Text py="4">
 
-          <Flex
-            justify="center"
-            align="center"
-            direction="row"
-            wrap="wrap"
-            m="1.5rem 0"
-          >
-            {Object.keys(tagColor).map((tag, index) => {
-              const color = tagColor[tag];
-
-              return (
-                <Box key={index}>
-                  <TagComponent color={color} onClick={() => filteredPosts(tag)}>
-                    {tag}
-                  </TagComponent>
-                </Box>
-              );
-            })}
-          </Flex>
-
-          {recipePost.length > 0 ? (
-            <RecipePost posts={recipePost} />
-          ) : (
-            <Alert
-              status="info"
-              borderRadius="md"
-              display="flex"
-              justifyContent="center"
-              mx="auto"
-              maxWidth="500px"
-              fontWeight="500"
-            >
-              <AlertIcon />
-              No blog post has been found!
-            </Alert>
-          )}
+          </Text>
         </Box>
       </motion.main>
     </>
   );
 };
 
-export async function getStaticProps() {
-  const data = await getAllFilesFrontMatter("recipes");
-  const posts = data.sort(
-    (a, b) => Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
-  );
-
-  return { props: { posts } };
-}
-
-export default Blog;
+export default Restaurants;
