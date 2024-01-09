78d8a4be5fe6b17e2b3172bd9f1f78d01c8b375e
Kairos Tay <hestiatay@gmail.com>
2024-01-09 09:30:49 +0800

    feat: reduce no. of tags (cleaner)

--- src/components/UI/tagColor.jsx
+++ src/components/UI/tagColor.jsx
@@ -4,9 +4,6 @@ export const tagColor = {
   Easy: { bgColor: "#86c34c", hover: "#5a8d33" }, // Greenish
   Medium: { bgColor: "#f48d07", hover: "#da7009" },
   Hard: { bgColor: "#E8265EE2", hover: "#ce0840E2" },
-  Quick: { bgColor: "#1DAC5AFF", hover: "#18904b" },
-  Moderate: { bgColor: "#f4ad07", hover: "#dc9406" },
-  "Time Consuming": { bgColor: "#b2475dE2", hover: "#933346E2" },
   Italian: { bgColor: "#865DCAE2", hover: "#784cc4E2" },
   Mexican: { bgColor: "#f11df1E2", hover: "#d313d3E2" },
   Indian: { bgColor: "#f06529", hover: "#e34c26" },
