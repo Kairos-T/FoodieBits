a5ca416e512f3666be26f17ca3db8af78780e7d4
Hu Bowen <contact@hubowen.dev>
2024-01-09 14:16:39 +0800

    fix: improve footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,83 +1,64 @@
-import { Box, Button, Icon, Link as ChakraLink, Text } from "@chakra-ui/react";
-import { FaCode, FaGithub, FaLinkedin } from "react-icons/fa";
+// Hu Bowen
+import { Box, Flex, Link, Text, useColorModeValue, VStack } from "@chakra-ui/react";
+import { MAX_WIDTH } from "../../config";
 
-import { MAX_WIDTH } from "config";
+import { footerData } from "../data/constants/footer";
 
-// Fell free to add your social media accounts!
-const socialAccounts = [
-  { icon: FaGithub, path: "https://github.com/", title: "Github" },
-  { icon: FaLinkedin, path: "https://www.linkedin.com/", title: "Linkedin" },
-  { icon: FaCode, path: "https://github.com/imadatyatalah/nextjs-chakra-ui-portfolio-template", title: "Source Code" }
-];
 
 const Footer = () => {
+  const footerColor = useColorModeValue("gray.100", "gray.900");
+
   return (
-    <Box as="footer">
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDir="column"
-        textAlign="center"
-        minH="4rem"
-        px={[4, 6, 10, 14, 20]}
-        maxW={MAX_WIDTH}
-        mx="auto"
+    <Box
+      width="100%"
+      backgroundColor={footerColor}
+    >
+      <VStack spacing={5} alignItems="initial"
+              maxW={MAX_WIDTH}
+              px={[4, 6, 10, 14, 20]}
+              marginTop={20}
+              padding={10}
+              mx="auto"
       >
-        <Box py="2">
-          {socialAccounts.map((item, index) => (
-            <ChakraLink
-              href={item.path}
-              aria-label={item.title}
-              mx="2"
-              _focus={{ outline: "none" }}
-              key={index}
-              isExternal
-            >
-              <Button aria-label={item.title}>
-                <Icon as={item.icon} w="6" h="6" />
-              </Button>
-            </ChakraLink>
+        <Flex
+          flexWrap="wrap"
+          direction={{ base: "column", md: "row" }}
+          alignItems="start"
+          justifyContent="space-between"
+        >
+          {footerData.map((data, index) => (
+            <Flex key={index} direction="column" mb="3">
+              <Link
+                fontWeight="500"
+                href={data.href}
+                color={useColorModeValue("gray.800", "gray.300")}
+              >
+                {data.label}
+              </Link>
+              <Flex direction={{ base: "row", md: "column" }}>
+                {data.links.map((link, index) => (
+                  <Link
+                    key={index}
+                    padding={1}
+                    fontSize={{ base: "sm", sm: "md" }}
+                    href="#"
+                    mr={{ base: 1, sm: 2, md: 0 }}
+                    color="gray.500"
+                    _hover={{ color: "blue.600" }}
+                  >
+                    {link.label}
+                  </Link>
+                ))}
+              </Flex>
+            </Flex>
           ))}
-        </Box>
-        <Box py="2">
-          <Text>
-            Built with{" "}
-            <span role="img" aria-label="red heart">
-              ❤️
-            </span>
-            ,{" "}
-            <ChakraLink
-              href="https://nextjs.org/"
-              fontWeight="600"
-              _focus={{ outline: "none" }}
-              isExternal
-            >
-              Next.js
-            </ChakraLink>
-            ,{" "}
-            <ChakraLink
-              href="https://chakra-ui.com/"
-              fontWeight="600"
-              _focus={{ outline: "none" }}
-              isExternal
-            >
-              Chakra UI
-            </ChakraLink>
-            , Hosted in{" "}
-            <ChakraLink
-              href="http://vercel.com/"
-              fontWeight="600"
-              _focus={{ outline: "none" }}
-              isExternal
-            >
-              Vercel.
-            </ChakraLink>
+        </Flex>
+        <Flex alignItems="center">
+          <Text color="gray.500" fontSize="0.875rem" pl="0.5rem">
+            &copy; 2024 FoodieBits. All rights reserved.
           </Text>
-        </Box>
-        <Box py="2">
-        </Box>
-      </Box>
+        </Flex>
+      </VStack>
     </Box>
   );
 };
