877968be50c460d46164143385497356595595ae
RaynaRuby <s10258808@connect.np.edu.sg>
2024-02-06 02:57:00 +0800

    chore: added share button to footer and modified footer

--- src/data/constants/index.jsx
+++ src/data/constants/index.jsx
@@ -62,10 +62,8 @@ const faqData = [
 
 export default faqData;
 
-// Footer data (links)
+// Footer data
 // By: Bowen & Ruby
-
-//I didn't push the pop-up yet since I haven't added all the requisites and it's kinda wonky atm 
 
 export const footerData = [
   {
@@ -82,7 +80,6 @@ export const footerData = [
     links: [
       { label: "Browse", href: "/recipes" },
       { label: "Suggest", href: "/contact" },
-      { label: "Share", href: "#"}
     ]
   },
   {
